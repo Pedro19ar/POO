3343de8a12680211dd6bfe159023a45c5b4f6221
Pedro19ar <paristizabala@unal.edu.co>
2023-02-24 18:20:28 -0500

    ayuda

---
+++ ayuda/src/main/java/com/mycompany/ayuda/Ayuda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ayuda;
+
+/**
+ *
+ * @author arist
+ */
+public class Ayuda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
