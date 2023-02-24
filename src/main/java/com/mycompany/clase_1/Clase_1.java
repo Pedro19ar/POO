0043e22b7540ae0eb33b88c7b6befe71acc84ebb
Pedro19ar <paristizabala@unal.edu.co>
2023-02-24 14:36:11 -0500

    primer repositorio

---
+++ src/main/java/com/mycompany/clase_1/Clase_1.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arist
+ */
+
+//          EJERCICIO 1
+
+public class Clase_1 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int edad_juan, edad_alberto, edad_ana, edad_mama;
+        
+        System.out.println("Digite la edad de Juan: ");
+        edad_juan = entrada.nextInt();
+        
+        edad_alberto = 2*(edad_juan/3);
+        edad_ana = 4*(edad_juan/3);
+        edad_mama = edad_juan+edad_alberto+edad_ana;
+        
+        System.out.println("La edad de Juan es: "+edad_juan+", La edad de alberto es: "+edad_alberto+", La edad de ana es: "+edad_ana+", La edad de mamá es: "+edad_mama);
+        
+        
+       
+        
+        
+        
+        
+    }
+}
